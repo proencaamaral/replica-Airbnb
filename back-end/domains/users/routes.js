d6e80c94010c25f465d0abce70a58339c87ba518
Luan Amaral <luaan.r@hotmail.com>
2025-06-17 06:40:27 -0300

    add conexion database ~ front

--- back-end/domains/users/routes.js
+++ back-end/domains/users/routes.js
@@ -5,12 +5,12 @@ import bcrypt from "bcryptjs"
 
 
 const router = Router();
-const bcryptSalt  = bcrypt.genSaltSync()
+const bcryptSalt = bcrypt.genSaltSync()
 
 router.get('/', async (req, res) => {
     connectDb()
     try {
-        
+
         const userDoc = await User.find();
         res.json(userDoc)
     } catch (error) {
@@ -19,13 +19,13 @@ router.get('/', async (req, res) => {
 
 });
 
-router.post("/",async (req, res) => {
+router.post("/", async (req, res) => {
     connectDb();
     const { name, email, password } = req.body;
     const encryptedPassword = bcrypt.hashSync(password, bcryptSalt);
 
     try {
-        
+
         const newUserDoc = await User.create({
             name,
             email,
@@ -34,9 +34,31 @@ router.post("/",async (req, res) => {
 
         res.json(newUserDoc);
     } catch (error) {
-        res.json(500).json(error);
+        res.status(500).json(error);
     }
 });
 
+router.post("/login", async (req, res) => {
+    connectDb();
+
+    const { email, password } = req.body;
+
+    try {
+        const userDoc = await User.findOne({ email });
+
+        if (userDoc) {
+            const passwordCorrect = bcrypt.compareSync(password, userDoc.password);
+            const { name, _id } = userDoc;
+
+            passwordCorrect ? res.json({ name,email, _id }) : res.status(400).json("Senha invalida !");
+
+        } else {
+            res.status(400).json("Usuário não encontrado !")
+        }
+
+    } catch (error) {
+        res.json(500).json(error);
+    }
+});
 
 export default router;
--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,14 @@
 import "dotenv/config"
 import express from 'express';
 import UserRoutes from "./domains/users/routes.js";
+import cors from "cors";
 
 
 const App = express();
 const { PORT } = process.env;
 
 
+App.use(cors());
 App.use(express.json());
 App.use('/users',UserRoutes)
 
